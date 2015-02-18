2f5691119ef4880e3d5cf2576fc46a8cbbb144f4
Mark Birbeck <mark.birbeck@gmail.com>
2015-02-18 22:14:41 +0000

    add generate command

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,8 @@ var del = require('del');
 
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var concat = require('gulp-concat');
+var filter = require('gulp-filter');
 
 
 /**
@@ -16,6 +18,8 @@ var gutil = require('gulp-util');
 
 var dir = {
   root: 'crawl',
+  crawl_generate: 'crawl_generate',
+  crawl_list: 'crawl_list'
 };
 
 /**
@@ -30,6 +34,14 @@ dir.crawldb = path.join(dir.root, 'crawldb');
 
 dir.seeds = path.join(dir.root, 'seeds');
 
+/**
+ * segments: A segment is a self-contained set of URLs, fetched content, parsed
+ * content, and so on. For now we are only supporting one segment:
+ */
+
+dir.segments = path.join(dir.root, 'segments');
+dir.segment1 = path.join(dir.segments, '1');
+
 
 /**
  * Class to handle crawl state:
@@ -39,6 +51,7 @@ var CrawlState = function (state){
   this.state = state || CrawlState.UNFETCHED;
 };
 CrawlState.UNFETCHED = 'unfetched';
+CrawlState.GENERATED = 'generated';
 
 /**
  * Clear the crawl database:
@@ -92,4 +105,83 @@ gulp.task('inject', ['clean:crawldb'], function (){
     }))
 
     .pipe(gulp.dest(dir.crawldb));
+});
+
+
+/**
+ * Clear the segments:
+ */
+
+gulp.task('clean:segments', function (cb){
+  del(dir.segments, cb);
+});
+
+/**
+ * Clear segment 1:
+ */
+
+gulp.task('clean:segment1', function (cb){
+  del(dir.segment1, cb);
+});
+
+
+/**
+ * generate: Place a list of URLs from the crawl database into a segment.
+ * For now we're only supporting one segment:
+ *
+ * See:
+ *
+ *  http://wiki.apache.org/nutch/bin/nutch%20generate
+ */
+
+gulp.task('generate', ['clean:segment1'], function (){
+  return gulp.src(path.join(dir.crawldb, '*'))
+
+    /**
+     * Save a bit of processing by only parsing the JSON once:
+     */
+
+    .pipe(es.map(function (file, cb){
+      file.crawlState = JSON.parse(file.contents.toString());
+
+      cb(null, file);
+    }))
+
+    /**
+     * Only process data sources that haven't been fetched yet:
+     */
+
+    .pipe(filter(function (file){
+      return file.crawlState.state === CrawlState.UNFETCHED;
+    }))
+
+    /**
+     * Update the status to indicate that the URL is about to be fetched:
+     */
+
+    .pipe(es.map(function (file, cb){
+      file.crawlState.state = CrawlState.GENERATED;
+      cb(null, file);
+    }))
+
+    /**
+     * Update the crawl database with any changes:
+     */
+
+    .pipe(es.map(function (file, cb){
+      file.contents = new Buffer(JSON.stringify( file.crawlState ));
+      cb(null, file);
+    }))
+    .pipe(gulp.dest(dir.crawldb))
+
+    /**
+     * Finally, create a crawl list:
+     */
+
+    .pipe(es.map(function (file, cb){
+      file.contents = new Buffer(decodeURIComponent(file.relative));
+      cb(null, file);
+    }))
+    .pipe(concat(dir.crawl_list))
+    .pipe(gulp.dest(path.join(dir.segment1, dir.crawl_generate)));
 });
