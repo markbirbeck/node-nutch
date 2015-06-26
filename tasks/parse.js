80b60b34a104e63c8cf20d532a67c7f6e606833e
Mark Birbeck <mark.birbeck@gmail.com>
2015-06-26 11:20:40 +0100

    refactor parse stage to use tika

--- tasks/parse.js
+++ tasks/parse.js
@@ -1,4 +1,5 @@
 var url = require('url');
+var path = require('path');
 
 var es = require('event-stream');
 var through2 = require('through2');
@@ -7,7 +8,9 @@ var filter = require('gulp-filter');
 
 var ParseState = require('../models/parseState');
 
-var cheerio = require('cheerio');
+var Tika = require('tika');
+var tika = new Tika();
+var config = require('../config/config');
 
 /**
  * parse: Parse content from fetched pages:
@@ -17,7 +20,7 @@ var cheerio = require('cheerio');
  *  https://wiki.apache.org/nutch/Nutch2Crawling#Parse
  */
 
-var parse = function (crawlBase, customParser, customParseChanged){
+var parse = function (crawlBase){
   var taskName = 'parse';
 
   return crawlBase.src()
@@ -31,72 +34,28 @@ var parse = function (crawlBase, customParser, customParseChanged){
         (!file.data.parseStatus ||
           file.data.parseStatus.state === ParseState.NOTPARSED);
     }))
-
-    /**
-     * Update the status to indicate that the URL is about to be fetched:
-     */
-
-    .pipe(es.map(function (file, cb){
-      var $ = cheerio.load(file.data.fetchedContent.content);
-      file.data.parse = {
-        title: $('title').text().trim(),
-        outlist: $('a').map(function (){
-          var title;
-          var _s;
-
-          if ($(this).attr('title')){
-            _s = '@title';
-            title = $(this).attr('title');
-          } else {
-            title = $(this).text().trim();
-            _s = 'text';
-            if (title === ''){
-              if ($('img', this)){
-                _s = 'img[@alt]';
-                title = $('img', this).attr('alt');
-              }
-            }
-          }
-
-          return {
-            url: url.resolve(file.data.url, $(this).attr('href') || ''),
-            _s: _s,
-            title: title
-          };
-        }).get()
-      };
-      file.data.parseStatus = new ParseState(ParseState.SUCCESS);
-      cb(null, file);
+    .pipe(es.map(function (file, doneParsing){
+      crawlBase.filesSrc(file.data.url, 'fetchedContent/content')
+        .pipe(tika)
+        .pipe(es.map(function(fetchedContent, cb) {
+          fetchedContent.base = config.dir.CrawlBase + path.sep;
+          fetchedContent.path = config.dir.CrawlBase + path.sep +
+            encodeURIComponent(file.data.url) + path.sep + 'parse';
+          file.data.parseStatus = new ParseState(ParseState.SUCCESS);
+          cb(null, fetchedContent);
+        }))
+        .pipe(crawlBase.filesDest())
+        .on('end', function() {
+          doneParsing(null, file);
+        });
     }))
-
-    /**
-     * Call any custom parser:
-     */
-
-    .pipe(es.map(function (file, cb){
-      if (customParser){
-        var customParse = customParser(file.data.fetchedContent.content);
-
-        if (customParse){
-
-          /**
-           * Use the provided function to check if the custom parse value has
-           * changed:
-           */
-
-          if (customParseChanged){
-            file.data.customParseChanged = customParseChanged(customParse, file.data.customParse);
-          }
-
-          /**
-           * Make sure to only overwrite old value after it has been used to
-           * check for changes:
-           */
-
-          file.data.customParse = customParse;
-          file.data.customParseStatus = new ParseState(ParseState.SUCCESS);
-        }
-      }
+    .pipe(through2.obj(function (file, enc, cb){
+      console.info(
+        '[%s] parsed \'%s\' (parse state=%s)',
+        taskName,
+        file.data.url,
+        file.data.parseStatus.state
+      );
       cb(null, file);
     }))
 
@@ -104,16 +63,6 @@ var parse = function (crawlBase, customParser, customParseChanged){
      * Update the crawl database with any changes:
      */
 
-    .pipe(through2.obj(function (file, enc, cb){
-      console.info(
-        '[%s] parsed \'%s\' (parse state=%s, custom parse state=%s)',
-        taskName,
-        file.data.url,
-        file.data.parseStatus.state,
-        file.data.customParseStatus.state
-      );
-      cb(null, file);
-    }))
     .pipe(crawlBase.dest());
 };
 
